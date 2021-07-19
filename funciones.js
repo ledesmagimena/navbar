0e3bebe69cef17303e64403b6ec22514c165c1a3
ledesma gimena <pledesma066@gmail.com>
2021-07-19 20:26:21 -0300

    copianav

--- funciones.js
+++ funciones.js
@@ -1,94 +1,30 @@
+import { navUsuario, usuariosSinR } from "./funcionesNav.js";
 const header = document.querySelector(".header");
 const navbar = document.createElement("div");
-let usuarios = true;
-function navPag(usuarios) {
-  if (usuarios === true) {
-    navbar.innerHTML = usuarios;
-    navbar.innerHTML = `<div class="bg-dark border border-danger rounded-pill">
-        <div class="container">
-          <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="font-weight-bold">
-  <img class="logo rounded-circle" src="./img/logo2.jpg" alt="logo">
-</div>
-            <form class="form-inline ml-5">
-              <input
-                class="form-control mr-sm-2"
-                type="search"
-                placeholder=""
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">
-                Buscar
-              </button>
-            </form>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navg navbar-nav w-100 justify-content-end">
-                
-                <a class="nav-link text-white" href="#">ayuda</a>
-                <a class="nav-link text-white" href="#">about us</a>
-                
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div>
-`;
+
+let log = false;
+localStorage.setItem("log", log);
+
+function navPag() {
+  let log = localStorage.getItem("log");
+  let usuario = JSON.parse(localStorage.getItem("userLogged"));
+  if (log === "true") {
+    if (usuario.rol === "usuario") {
+      navbar.innerHTML = navUsuario(usuario);
+      header.append(navbar);
+      return;
+    } /*else {
+    }*/
   } else {
-    navbar.innerHTML = usuarios;
-    navbar.innerHTML = `<div class="bg-dark border border-danger rounded-pill">
-        <div class="container">
-          <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="font-weight-bold">
-  <img class="logo rounded-circle" src="./img/logo2.jpg" alt="logo">
-</div>
-            <form class="form-inline ml-5">
-              <input
-                class="form-control mr-sm-2"
-                type="search"
-                placeholder=""
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">
-                Buscar
-              </button>
-            </form>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navg navbar-nav w-100 justify-content-end">
-                <a class="nav-link text-white active" href="#">iniciar sesion</a>
-                <a class="nav-link text-white" href="#">ayuda</a>
-                <a class="nav-link text-white" href="#">about us</a>
-                <a class="nav-link text-white">registrarse</a>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div>
-`;
+    navbar.innerHTML = usuariosSinR(usuario);
+    console.log(navbar);
+    header.append(navbar);
+    return;
   }
-  /*return navPag;*/
 }
 
-header.append(navbar);
 navPag();
+/*
+bygbyg.addEvenListenner((e){
+localStorage.setItem("log", "false")
+})*/
